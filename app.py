b4ae1ec26b0e6cb13d23a51995787f5bda048379
Flame <flameyungoxx@gmail.com>
2022-06-24 18:56:13 +0400

    [Feature] Added login route

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def hello_world():
 def register():
     return "This is register page" # TODO Generalize
 
+@app.route('/login')
+def login():
+    return "This is login page" # TODO Generalize
+
 if __name__ == '__main__':
     app.run(debug=True, port=7777)
