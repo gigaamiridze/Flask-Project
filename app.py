fc1d111a0e456cdb6d20e85748c26d8a34a734ff
Flame <flameyungoxx@gmail.com>
2022-06-24 18:04:41 +0400

    Added register route

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
+
+@app.route('/register')
+def register():
+    return "This is register page" # TODO Generalize
 
 if __name__ == '__main__':
     app.run(debug=True, port=7777)
